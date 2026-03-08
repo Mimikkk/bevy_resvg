f6495b6fcbc97d5eaf77243fbbd8fa513700ce90
Theodore Bjernhed <uik1475@edu.kunskapsskolan.se>
2026-03-08 22:06:54 +0100

    feat: add `usvg::Options` support

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -1,9 +1,13 @@
+pub mod options;
+
+use crate::settings::options::OptionsDef;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Serialize, Deserialize, Clone)]
 #[non_exhaustive]
 pub struct SvgFileLoaderSettings {
     pub target_render_size: Option<TargetRenderSize>,
+    pub options: OptionsDef,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, Copy)]
